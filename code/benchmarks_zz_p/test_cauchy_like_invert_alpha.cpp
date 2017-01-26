c963e8bf7c4954861b76e28fbe0f79a49abb9ae3
Eric Schost <eschost@uwaterloo.ca>
2017-01-26 00:39:44 -0500

    bench

--- code/benchmarks_zz_p/test_cauchy_like_invert_alpha.cpp
+++ code/benchmarks_zz_p/test_cauchy_like_invert_alpha.cpp
@@ -16,9 +16,65 @@ void check(int opt){
   else
     zz_p::UserFFTInit(65537);
 
+  long lo = 100;
+  long hi = 10000;
+  
   cout << "# p=" << zz_p::modulus() << endl;
 
-   for (long i = 9900; i < 10000; i += 200){
+   for (long i = lo; i < hi; i += 200){
+
+     long NB = 1;
+     if (i < 1000)
+       NB = 5;
+     if (i < 800)
+       NB = 10;
+     if (i < 700)
+       NB = 20;
+     if (i < 600)
+       NB = 60;
+     if (i < 500)
+       NB = 125;
+     if (i < 400)
+       NB = 250;
+     if (i < 300)
+       NB = 500;
+     if (i < 200)
+       NB = 1000;
+     if (i < 100)
+       NB = 2000;
+     if (i < 50)
+       NB = 5000;
+
+     long t_alpha[] = {5, 50};
+
+     for (long idx = 0; idx < 2; idx++){
+       long alpha = t_alpha[idx];
+       double t1 = 0;
+       
+       zz_p a = to_zz_p(9);
+       mat_zz_p A, B;
+       random(A, i, alpha);
+       random(B, i, alpha);
+       lzz_p_cauchy_like_geometric M(A, B, to_zz_p(1), power(a, i), a);
+       
+       cout << i << " " << alpha << " ";
+       
+       t1 = GetTime();
+       for (long k = 0; k < NB; k++){
+	 lzz_p_cauchy_like_geometric Minv;
+	 invert(Minv, M);
+       }
+       t1 = GetTime() - t1;
+       t1 = t1/NB;
+       cout << t1 << " ";
+       
+       cout << endl;
+     }
+     cout << endl;
+   }
+
+
+   for (long i = lo; i < hi; i += 200){
 
      long NB = 1;
      if (i < 1000)
@@ -50,34 +106,8 @@ void check(int opt){
      t0 = GetTime() - t0;
      t0 = t0/NB;
      cout << i << " " << 0 << " " << t0 << endl;
-
-     long t_alpha[] = {5, 50};
-
-     for (long idx = 0; idx < 2; idx++){
-       long alpha = t_alpha[idx];
-       double t1 = 0;
-       
-       zz_p a = to_zz_p(9);
-       mat_zz_p A, B;
-       random(A, i, alpha);
-       random(B, i, alpha);
-       lzz_p_cauchy_like_geometric M(A, B, to_zz_p(1), power(a, i), a);
-       
-       cout << i << " " << alpha << " ";
-       
-       t1 = GetTime();
-       for (long k = 0; k < NB; k++){
-	 lzz_p_cauchy_like_geometric Minv;
-	 invert(Minv, M);
-       }
-       t1 = GetTime() - t1;
-       t1 = t1/NB;
-       cout << t1 << " ";
-       
-       cout << endl;
-     }
-     cout << endl;
    }
+
 }
 
 /*------------------------------------------------------------*/
