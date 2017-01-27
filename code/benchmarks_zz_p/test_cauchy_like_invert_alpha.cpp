fe78e06a88e60c1aa6dc5c88690e70b5cabfd540
schost <eschost@eschost-desktop>
2017-01-27 12:30:30 -0500

    benchmark files

--- code/benchmarks_zz_p/test_cauchy_like_invert_alpha.cpp
+++ code/benchmarks_zz_p/test_cauchy_like_invert_alpha.cpp
@@ -11,17 +11,18 @@ NTL_CLIENT
 /* else, runs mod 65537                                       */
 /*------------------------------------------------------------*/
 void check(int opt){
+
   if (opt == 1)
     zz_p::FFTInit(0);
   else
     zz_p::UserFFTInit(65537);
 
   long lo = 100;
-  long hi = 10000;
+  long hi = 5000;
   
   cout << "# p=" << zz_p::modulus() << endl;
 
-   for (long i = lo; i < hi; i += 200){
+   for (long i = lo; i < hi; i += 100){
 
      long NB = 1;
      if (i < 1000)
@@ -62,7 +63,7 @@ void check(int opt){
        t1 = GetTime();
        for (long k = 0; k < NB; k++){
 	 lzz_p_cauchy_like_geometric Minv;
-	 invert(Minv, M);
+	 invert_fast(Minv, M);
        }
        t1 = GetTime() - t1;
        t1 = t1/NB;
@@ -71,41 +72,6 @@ void check(int opt){
        cout << endl;
      }
      cout << endl;
-   }
-
-
-   for (long i = lo; i < hi; i += 200){
-
-     long NB = 1;
-     if (i < 1000)
-       NB = 5;
-     if (i < 800)
-       NB = 10;
-     if (i < 700)
-       NB = 20;
-     if (i < 600)
-       NB = 60;
-     if (i < 500)
-       NB = 125;
-     if (i < 400)
-       NB = 250;
-     if (i < 300)
-       NB = 500;
-     if (i < 200)
-       NB = 1000;
-     if (i < 100)
-       NB = 2000;
-     if (i < 50)
-       NB = 5000;
-
-     Mat<zz_p> check, check_i;
-     random(check, i, i);
-     double t0 = GetTime();
-     for (long j = 0; j < NB; j++)
-       check_i = inv(check);
-     t0 = GetTime() - t0;
-     t0 = t0/NB;
-     cout << i << " " << 0 << " " << t0 << endl;
    }
 
 }
